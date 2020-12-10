30cadd19ab2d7f3eeda2e62a25de288c4b554a2f
NLSteveO <steveokeefe88@gmail.com>
2020-12-10 01:09:30 -0330

    2020 Day 5 - Part 1

---
+++ 2020/day05/puzzle1.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const path = require('path');
+
+const numberOfRows = 128;
+const numberOfColumns = 8;
+const keys = {
+  F: 0,
+  B: 1,
+  L: 0,
+  R: 1
+};
+
+const calculateSeatId = (row, column) => row * 8 + column;
+
+const getHalves = (min, max) => {
+  const diff = max - min;
+  const middle = min + Math.round(diff/2);
+  const lowerHalf = [
+    min,
+    middle
+  ];
+  const upperHalf = [
+    middle,
+    max
+  ];
+  return [lowerHalf, upperHalf];
+};
+
+const getRow = rowPartitions => {
+  let rows = [0, numberOfRows - 1];
+  rowPartitions.forEach(val => {
+    rows = getHalves(rows[0], rows[1])[keys[val]];
+  });
+  return rows[0];
+}
+
+const getColumn = columnPartitions => {
+  let columns = [0, numberOfColumns - 1];
+  columnPartitions.forEach(val => {
+    columns = getHalves(columns[0], columns[1])[keys[val]];
+  });
+  return columns[0];
+}
+
+const getAnswer = (data) => {
+  const seatIds = data.map(boardingPass => {
+    const rowPartitions = boardingPass.substring(0, 7);
+    const columnPartitions = boardingPass.substring(7, boardingPass.length)
+    const row = getRow(rowPartitions.split(''));
+    const column = getColumn(columnPartitions.split(''));
+    return calculateSeatId(row, column);
+  });
+  return Math.max(...seatIds);
+}
+
+const getFile = fileName => {
+  const filePath = path.join(__dirname, fileName);
+   fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
+    if (!err) {
+      const answer = getAnswer(data.split('\n'));
+      console.log(answer);
+    } else {
+      console.error(err);
+      return;
+    }
+  });
+}
+
+getFile(process.argv[2]);
