a786b962c0bc591e9a46f3f65ce3fb742d6d24ca
NLSteveO <steveokeefe88@gmail.com>
2021-12-01 23:51:30 -0330

    Add script to setup a day

---
+++ scripts/templates/puzzle.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const getAnswer = () => {
+}
+
+const getFile = fileName => {
+  const filePath = path.join(__dirname, fileName);
+   fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
+    if (!err) {
+      const answer = getAnswer(data.split('\n'));
+      console.log(answer);
+    } else {
+      console.error(err);
+      return;
+    }
+  });
+}
+
+getFile(process.argv[2]);
