01cab5eddb8df9ceb31fdad06a1a5d343b68cdf5
NLSteveO <steveokeefe88@gmail.com>
2021-12-10 01:53:58 -0330

    2021 Day 2

---
+++ 2021/day02/puzzle2.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+const getAnswer = (data) => {
+  let horizontalPosition = 0;
+  let depth = 0;
+  let aim = 0;
+
+  data.forEach(command => {
+    const [direction, amount] = command.split(' ');
+    if (direction === 'forward') {
+      horizontalPosition += Number(amount);
+      depth += (Number(amount) * aim);
+    } else if (direction === 'up') {
+      aim -= Number(amount);
+    } else {
+      aim += Number(amount);
+    }
+  });
+
+  return horizontalPosition * depth;
+}
+
+const getFile = fileName => {
+  const filePath = path.join(__dirname, fileName);
+   fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
+    if (!err) {
+      const answer = getAnswer(data.split('\n'));
+      console.log(answer);
+    } else {
+      console.error(err);
+      return;
+    }
+  });
+}
+
+getFile(process.argv[2]);
