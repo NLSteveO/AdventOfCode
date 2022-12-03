c8f2ee5443e93c4dfd67538b319e046ada7888bd
NLSteveO <steveokeefe88@gmail.com>
2022-12-03 14:13:32 -0330

    2022 Day 1

---
+++ 2022/day01/puzzle2.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const getAnswer = (data) => {
+    const elfList = [];
+    let elfIndex = 0;
+    data.forEach(calories => {
+        if (calories === '') elfIndex++;
+        else {
+            if (!elfList[elfIndex]) elfList[elfIndex] = 0;
+            elfList[elfIndex] = elfList[elfIndex] + parseInt(calories);
+        }
+    });
+    elfList.sort((a, b) => b - a);
+    // console.log(elfList[0], e);
+    return elfList[0] + elfList[1] + elfList[2];
+}
+
+const getFile = fileName => {
+  const filePath = path.join(__dirname, fileName);
+   fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
+    if (!err) {
+      const answer = getAnswer(data.split('\n'));
+      console.log(answer);
+    } else {
+      console.error(err);
+      return;
+    }
+  });
+}
+
+getFile(process.argv[2]);
