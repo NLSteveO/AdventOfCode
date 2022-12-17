b6dedac10aa1314db75b9eb58bf8929715718202
NLSteveO <steveokeefe88@gmail.com>
2022-12-16 22:13:27 -0330

    2022 Day 6

---
+++ 2022/day06/puzzle1.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+
+const hasDuplicates = (sequence) => {
+    let hasDuplicate = false;
+    for(let i = 0; i < sequence.length; i++) {
+        if (sequence.indexOf(sequence[i], i+1) >= 0) {
+            hasDuplicate = true;
+            break;
+        }
+    }
+    return hasDuplicate;
+}
+
+const getAnswer = (data) => {
+    let index = 0;
+    for (let i = 0; i < data.length - 3; i++) {
+        const sequence = data.substring(i, i+4);
+        if (!hasDuplicates(sequence)) {
+            index = i+4;
+            break;
+        }
+    }
+    return index;
+}
+
+const getFile = fileName => {
+  const filePath = path.join(__dirname, fileName);
+   fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
+    if (!err) {
+      const answer = getAnswer(data.split('\n')[0]);
+      console.log(answer);
+    } else {
+      console.error(err);
+      return;
+    }
+  });
+}
+
+getFile(process.argv[2]);
